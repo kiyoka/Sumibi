53dec213b14af6bd36319995487b96e08cf8bc97
unknown <kiyoka@sumibi.org>
2023-04-17 22:05:22 +0900

    Update romaji_kanji_converter1.py
    パラメーターを修正。

--- playground/romaji_kanji_converter1.py
+++ playground/romaji_kanji_converter1.py
@@ -9,8 +9,8 @@ openai.api_key = os.getenv("OPENAI_API_KEY")
 response = openai.ChatCompletion.create(
   model="gpt-3.5-turbo",
   messages=[
-        {"role": "system", "content": "あなたはローマ字をかなと漢字からなる日本語に変換するアシスタントです"},
-        {"role": "user", "content": 'convert the following romaji : "{watashi ha nihongo wo syaberu kotoga dekimasu .}"'}
+        {"role": "system", "content": "あなたはローマ字と日本語を変換するアシスタントです。"},
+        {"role": "user", "content": 'ローマ字の文を漢字仮名混じり文にしてください。 : "{watashi ha nihongo wo hanasukotoga dekimasu .}"'}
     ]
 )
 print(response['choices'][0]['message']['content'])
