9510e95d7249f9cd2b849708bed624c6fc5454ef
Kiyoka Nishiyama <kiyoka@sumibi.org>
2023-04-19 21:24:49 +0900

    saved roman to kanji result as text file.
    gpt-3.5-turboモデルでのローマ字漢字変換の結果をファイルに保存できるようにした。

---
+++ playground/make.bat
@@ -0,0 +1,2 @@
+
+python.exe romaji_kanji_convert_sentences.py gpt-3.5-turbo > gpt-3.5-turbo.txt
--- playground/romaji_kanji_convert_sentences.py
+++ playground/romaji_kanji_convert_sentences.py
@@ -3,36 +3,12 @@ import os
 import io
 import sys
 import openai
-
 sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8')
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
-def kanji_to_yomigana(kanji):
-    response = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo",
-        messages=[
-            {"role": "system", "content":
-             "あなたは漢字が与えられると、ひらがなに変換するアシスタントです。"
-             },
-            {"role": "user", "content": '次をひらがなのみで表記してください。 : "{0}"'.format(kanji)}
-        ]
-    )
-    return(response['choices'][0]['message']['content'])
-    
-    
-def romaji_to_kanji(romaji):
-    response = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo",
-        messages=[
-            {"role": "system", "content":
-             "あなたはローマ字と日本語を変換するアシスタントです。"
-             },
-            {"role": "user", "content": 'ローマ字の文を漢字仮名混じり文にしてください。 : "{0}"'.format(romaji)}
-        ]
-    )
-    return(response['choices'][0]['message']['content'])
 
-romaji_list = [
+class GptTest:
+    romaji_list = [
     'watashi no namae ha nakano desu .',
     'watashinonamaehanakanodesu .',
     '1nen ha 1gatu3ka kara hajimarimasu .',
@@ -45,23 +21,65 @@ romaji_list = [
     'watashi ha nihongo wo syaberu kotoga dekimasu .',
     ]
 
-kanji_list = [
-    '漢字',
-    '東西南北',
-    '行う',
-    '東京特許許可局'
-]
+    kanji_list = [
+        '漢字',
+        '東西南北',
+        '行う',
+        '東京特許許可局'
+    ]
 
-if True:
-    for romaji in romaji_list:
-        print('IN  : {0}'.format(romaji))
-        print('OUT1: {0}'.format(romaji_to_kanji(romaji)))
-        print('OUT2: {0}'.format(romaji_to_kanji(romaji)))
-        print()
+    model = "gpt-3.5-turbo"
 
-if True:
-    for kanji in kanji_list:
-        print('IN  : {0}'.format(kanji))
-        print('OUT1: {0}'.format(kanji_to_yomigana(kanji)))
-        print('OUT2: {0}'.format(kanji_to_yomigana(kanji)))
-        print()
+    def set_model(self,model):
+        self.model = model
+
+    def kanji_to_yomigana(self,kanji):
+        response = openai.ChatCompletion.create(
+            model=self.model,
+            messages=[
+                {"role": "system", "content":
+                "あなたは漢字が与えられると、ひらがなに変換するアシスタントです。"
+                },
+                {"role": "user", "content": '次をひらがなのみで表記してください。 : "{0}"'.format(kanji)}
+                ]
+            )
+        return(response['choices'][0]['message']['content'])
+    
+    
+    def romaji_to_kanji(self,romaji):
+        response = openai.ChatCompletion.create(
+            model=self.model,
+            messages=[
+                {"role": "system", "content":
+                 "あなたはローマ字と日本語を変換するアシスタントです。"
+                },
+                {"role": "user", "content": 'ローマ字の文を漢字仮名混じり文にしてください。 : "{0}"'.format(romaji)}
+            ]
+        )
+        return(response['choices'][0]['message']['content'])
+
+    def romaji_func(self):
+        for romaji in self.romaji_list:
+            print('IN  : {0}'.format(romaji))
+            print('OUT1: {0}'.format(self.romaji_to_kanji(romaji)))
+            print('OUT2: {0}'.format(self.romaji_to_kanji(romaji)))
+            print('OUT3: {0}'.format(self.romaji_to_kanji(romaji)))
+            print()
+
+    def kanji_func(self):
+        for kanji in self.kanji_list:
+            print('IN  : {0}'.format(kanji))
+            print('OUT1: {0}'.format(self.kanji_to_yomigana(kanji)))
+            print('OUT2: {0}'.format(self.kanji_to_yomigana(kanji)))
+            print('OUT3: {0}'.format(self.kanji_to_yomigana(kanji)))
+            print()
+
+def main(argv):
+    gptTest = GptTest()
+    if(1 < len(argv)):
+        gptTest.set_model(argv[1])
+    gptTest.romaji_func()
+    gptTest.kanji_func()
+
+if __name__ == "__main__":
+     main(sys.argv)
