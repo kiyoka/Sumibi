ee95aebf88591b4bc7281ec0fda8d92245b4d6d1
Kiyoka Nishiyama <kiyoka@sumibi.org>
2025-05-11 16:15:34 +0900

    ４モデルの誤変換率のプロットが完成。

--- benchmark/plot_cer_vs_cost.py
+++ benchmark/plot_cer_vs_cost.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 変換エラー率とコストの比較グラフをプロットする
 """
 modules = ['gpt-4.1-mini', 'gpt-4.1', 'gpt-4o-mini', 'gpt-4o']
-error_rate = [0, 0, 0, 0]      # 単位: %
+error_rate = [0.430172, 0.218878, 0.735027, 0.197426]      # 単位: %
 costs = [0.00052, 0.00260, 0.000195, 0.00550]              # 単位: $/リクエスト ( sumibi_typical_convert_client.py でリクエスト500トークン、レスポンス200トークンを消費すると仮定した)
 
 # 散布図の描画とモデル名の表示
@@ -13,7 +13,7 @@ colors = ['skyblue', 'orange', 'red', 'green']
 for name, cost, err, c in zip(modules, costs, error_rate, colors):
     plt.scatter(cost, err, s=150, color=c)
     plt.annotate(name,
-                 xy=(cost, err),
+                 xy=(cost, err * 100),
                  xytext=(5, 5),
                  textcoords='offset points',
                  ha='left',
