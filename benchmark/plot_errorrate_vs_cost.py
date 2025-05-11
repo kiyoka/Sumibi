d328eb3ae7a61cbb05509cb4afdbddb8b1bccb6b
Kiyoka Nishiyama <kiyoka@sumibi.org>
2025-05-11 22:00:54 +0900

    リファクタリングとインデント修正。

--- benchmark/plot_errorrate_vs_cost.py
+++ benchmark/plot_errorrate_vs_cost.py
@@ -3,14 +3,35 @@ import matplotlib.pyplot as plt
 """
 変換エラー率とコストの比較グラフをプロットする
 """
-modules = ['gpt-4.1-mini', 'gpt-4.1', 'gpt-4o-mini', 'gpt-4o', 'o4-mini', 'deepseek-v3']
-error_rate = [0.430172, 0.218878, 0.735027, 0.197426, 0.332583, 0.592557]      # 単位: %
-costs = [0.00052, 0.00260, 0.000195, 0.00550, 0.00143, 0.000355]              # 単位: $/リクエスト ( sumibi_typical_convert_client.py でリクエスト500トークン、レスポンス200トークンを消費すると仮定した)
+models = [
+    'gpt-4.1-mini',
+    'gpt-4.1',
+    'gpt-4o-mini',
+    'gpt-4o',
+    'o4-mini',
+    'deepseek-v3'
+]
+error_rate = [ # 単位: %
+    0.430172,
+    0.218878,
+    0.735027,
+    0.197426,
+    0.332583,
+    0.592557
+]
+costs = [ # 単位: $/リクエスト ( sumibi_typical_convert_client.py でリクエスト500トークン、レスポンス200トークンを消費すると仮定した)
+    0.00052,
+    0.00260,
+    0.000195,
+    0.00550,
+    0.00143,
+    0.000355
+]
 
 # 散布図の描画とモデル名の表示
 plt.figure(figsize=(8, 6))
 colors = ['skyblue', 'orange', 'gray', 'green', 'pink', 'red']
-for name, cost, err, c in zip(modules, costs, error_rate, colors):
+for name, cost, err, c in zip(models, costs, error_rate, colors):
     # Error Rate をパーセント表示に変換してプロット
     pct = err * 100
     plt.scatter(cost, pct, s=150, color=c)
